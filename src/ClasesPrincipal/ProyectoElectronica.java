12d8b751477a677fc7cbdec11c9be74874922896
alejandro Gomez <alejandro.gomezg@autonoma.edu.co>
2022-05-12 19:34:48 -0500

    Se cambia texto de subida de archivo

--- src/ClasesPrincipal/ProyectoElectronica.java
+++ src/ClasesPrincipal/ProyectoElectronica.java
@@ -5,6 +5,14 @@
  */
 package ClasesPrincipal;
 
+import ClasesArchivos.Escritor;
+import ClasesArchivos.Lector;
+import ClasesArchivos.LectorEscritorTextoPlano;
+import G.U.I.VentanaPrincipal;
+import java.io.File;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author JUAN MORA
@@ -14,8 +22,33 @@ public class ProyectoElectronica {
     /**
      * @param args the command line arguments
      */
+    String nombreArchivo;
     public static void main(String[] args) {
-        String sapa;
+            
+         ProyectoElectronica proyecto = new ProyectoElectronica();
+        Lector lector = new LectorEscritorTextoPlano();
+        Escritor escritor = new LectorEscritorTextoPlano();
+        VentanaPrincipal interfaz = new VentanaPrincipal(proyecto, lector);
+        interfaz.setVisible(true);
+        
+    }
+     public ArrayList<String> cargarArchivo(Lector lector) {
+        File archivo = lector.seleccionarArchivo();
+        setNombreArchivo(archivo.getName());
+        if(archivo.getName().endsWith("txt")){
+            ArrayList<String> elementos = lector.leerArchivo(archivo);
+            return elementos;    
+        }else{
+            JOptionPane.showMessageDialog(null, "Este archivo no es aceptado, selecciona un archivo .txt.");
+        }
+        return null;
+    }
+    public void setNombreArchivo(String nombreArchivo){
+        this.nombreArchivo = nombreArchivo;
+    }
+
+    public String getNombreArchivo() {
+        return nombreArchivo;
     }
     
 }
