28a9703b8d9e7f8c4b6f9572d9473c2a8668eaa4
alejandro Gomez <alejandro.gomezg@autonoma.edu.co>
2022-05-24 18:17:55 -0500

    Se crea la clase Dato que es donde se van a guardar los valores

---
+++ src/G/U/I/Dato.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package G.U.I;
+
+/**
+ *
+ * @author Alejandro Gomez G
+ */
+public interface Dato {
+    String nombre;
+    String valorBinario;
+    float valor;
+
+    public Dato(String nombre, String valorBinario, float valor) {
+        setNombre(nombre);
+        setValorBinario(valorBinario);
+        setValor(valor);
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getValorBinario() {
+        return valorBinario;
+    }
+
+    public void setValorBinario(String valorBinario) {
+        this.valorBinario = valorBinario;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+}
